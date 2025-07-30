e3be3eded49fe7e4f4185a106bea4a59f2963969
TechWritingFitz <119666232+TechWritingFitz@users.noreply.github.com>
2025-07-30 11:35:21 +0100

    Fix HTML file path - serve from STATIC folder

--- simple_app.py
+++ simple_app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('STATIC', 'index.html')
 
 @app.route('/static/<path:filename>')
 def static_files(filename):
-    return send_from_directory('static', filename)
+    return send_from_directory('STATIC', filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
